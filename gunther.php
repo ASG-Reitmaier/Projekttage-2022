88cdc01a6117e94b9a06454b21387b97d1e2494a
Hamza Tamim <hamza.tamim@asg-passau.de>
2021-07-09 16:50:07 +0200

    test

--- gunther.php
+++ gunther.php
@@ -6,4 +6,5 @@ $sql_02 = "INSERT INTO benutzer (Benutzer_ID, Name, Jahrgangsstufe, Rolle) VALUE
 $pdo->query($sql_02)
 $sql_03 = "UPDATE benutzer SET Benutzer_ID = 3, Jahrgangsstufe = 7 WHERE Name = 'Clara Bauer'";
 $pdo->query($sql_03)
+
 ?>
