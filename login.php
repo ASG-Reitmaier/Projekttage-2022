e8d5c7ca9fd8a1d60dcbef42b07758a04fb4ec4e
Christian Reitmaier <christian.reitmaier@asg-passau.de>
2021-07-12 17:48:34 +0200

    login.php hinzugefügt

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+ 
+    <head>
+        
+        <meta name ="viewport" content="width-device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>Anmeldemaske</title>
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        
+        
+    </head>
+    <body>
+        
+        <div style="float: right; background-color:#fb4400; height: 1042px; width:4%" >
+        
+        </div>
+        
+        <img src="Logo-Desktop.jpg" style="padding: 2%; width:25%;height:auto;">
+        
+        <br>
+         <br>
+         <br>
+         <br>
+            
+        <h1 style="text-align: center; font-family: sans-serif">Anmelden</h1>
+       
+        
+       
+       <div style= "background-color: white; width: 60%; height: auto; margin: auto; border-style: solid; border-color: #f2f2f2; border-width: 12px; border-radius: 8px">
+                <br>
+                <br>
+            <div style=" padding-left: 3%; padding-right: 3%" class="mb-3">
+                    <label for="formGroupExampleInput" class="form-label" style="font-family:sans-serif">Benutzername</label>
+                    <input style="border-style:solid; border-color:#e9e9e9; border-width: 3px; ;background-color: #f2f2f2" type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+            </div>
+            
+            <div style=" padding-left: 3%; padding-right: 3%" class="mb-3">
+                    <label for="formGroupExampleInput2" class="form-label" style="font-family:sans-serif">Passwort</label>
+                    <input type="password" style="border-style:solid; border-color:#e9e9e9; border-width: 3px; ;background-color: #f2f2f2" type="text" class="form-control" id="eformGroupExampleInput2" placeholder="">
+            </div>
+              
+              <br>
+              
+            <a href="" style="text-align: center; margin: 42%;"> <button type="button" class="btn btn-light" style="background-color:#fb4400; color: white; font-size:21px;  width: 150px">Anmelden</button></a>
+            <br>
+           <br>
+        </div>
+        
+        
+    </body>
+    
+</html>
