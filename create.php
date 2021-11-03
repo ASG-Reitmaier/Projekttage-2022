3baf9fe6683dd3b0b3fe0801dc35703ffa474a8f
Peter-Jar <Peter.Jarzombek@asg-passau.de>
2021-11-03 22:43:04 +0100

    create.php Seite erstellt

---
+++ create.php
@@ -0,0 +1,84 @@
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Projekterstellung</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/grid/">
+
+    <!-- CSS von Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Grid-Design von Bootstrap -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/grid/grid.css" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    
+
+ 
+    <h1 align = "center">Projekterstellung</h1><br> <br>
+    
+    <form action="search.php" method="post">
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input class="form-control" id="name" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Beschreibung</label>
+    <textarea class="form-control" id="beschreibung" placeholder="" rows="3"></textarea>
+  </div>
+  
+  <div class="form-group">
+    <label for="name">Kursleiter1</label>
+    <input class="form-control" id="kursleiter1" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Kursleiter2</label>
+    <input class="form-control" id="kursleiter2" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Kursleiter3</label>
+    <input class="form-control" id="kursleiter3" placeholder="">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Teilnehmerbegrenzung</label>
+    <input type="number" class="form-control" id="teilnehmerbegrenzung" min="1" max="30" step="1" value="10" >
+  </div>
+</form>
+  </body>
+</html>
+<!-- <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+-->
