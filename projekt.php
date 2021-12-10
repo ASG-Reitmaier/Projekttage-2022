e14053df905f873252347cd30f79e5d1d92a1307
Peter-Jar <peter.jarzombek@asg-passau.de>
2021-12-10 08:59:24 +0100

    Probleme

--- projekt.php
+++ projekt.php
@@ -36,9 +36,10 @@ if(isset($_GET['id'])){
     $projektDaten = $db->zeigeKurs($_GET['id']);
     echo $projektDaten[0]["name"];
     ?>
+    
 
    <div>
-   <img src= '<?php echo $projektDaten[0]['bild']?>' class='img-thumbnail' style='max-width:50%' alt='Responsive image'>
+   <img src= '<?php echo $projektDaten[0]['bild'];?>' class='img-thumbnail' style='max-width:50%' alt='Responsive image'>
    <?php
    echo $projektDaten[0]["beschreibung"];?>
    <div>
