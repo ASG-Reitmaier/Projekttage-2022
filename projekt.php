185d83dd7afb48dee6c233cd1077891748047d26
Jonas Kolbeck <jonas.kolbeck@asg-passau.de>
2022-01-14 09:24:58 +0100

    update

--- projekt.php
+++ projekt.php
@@ -25,7 +25,7 @@ $db = new DB();
 <body>
 
 
-<img src="img/asg-logo.jpg" class="img-fluid" alt="Responsive image">
+
   
 
 
@@ -37,39 +37,32 @@ if(isset($_GET['id'])){
     $projektDaten = $db->zeigeKurs($_GET['id']);
     
     ?>
-    
-
-<<<<<<< HEAD
-<h1 align ="center"> <?php echo $projektDaten[0]["name"]; ?></h1>
-=======
+ 
+<h1 font-family="mono sans" align ="center"> <?php echo $projektDaten[0]["name"]; ?></h1>
+<br>
    <div>
-   <img src= '<?php echo "../".$projektDaten[0]['bild'];?>' class='img-thumbnail' style='max-width:25%' alt='Responsive image'>
-   <?php
-   echo $projektDaten[0]["beschreibung"];?>
+   <img src= '<?php echo "../".$projektDaten[0]['bild'];?>' class='rounded mx-auto d-block img-thumbnail' style='max-width:30%' alt='Responsive image'>
+  
    <div>
->>>>>>> 953e63610f21f6806d36de323e6b301cffa2915a
-
-<div class="container-md">
-  <img src= '<?php echo $projektDaten[0]['bild']?>' class=' rounded mx-auto d-block' style='max-width:50%' alt='Responsive image'> 
-</div>
 
   <div class="container-sm p-5 my-5 border">
    <h5 align ="center"> <?php echo $projektDaten[0]["beschreibung"];?> </h5>
   </div>
    <div class="container-md">
   <table class="table table-borderless table-active table-vcenter">
+
   <thead>
     <tr>
-      <th scope="col">Kursleiter 1</th>
-      <th scope="col">Kursleiter 2</th>
-      <th scope="col">Kursleiter 3</th>
+      <th class="text-center" scope="col">Kursleiter 1</th>
+      <th class="text-center" scope="col">Kursleiter 2</th>
+      <th class="text-center" scope="col">Kursleiter 3</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>     <?php echo $projektDaten[0]["kursleiter1"] ?></td>
-      <td>     <?php echo $projektDaten[0]["kursleiter2"] ?></td>
-      <td>     <?php echo $projektDaten[0]["kursleiter3"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["kursleiter1"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["kursleiter2"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["kursleiter3"] ?></td>
     </tr>
   </tbody>
 </table> 
@@ -82,25 +75,46 @@ if(isset($_GET['id'])){
 
 
   <table class="table table-borderless table-active table-vcenter">
+ 
   <thead>
     <tr>
-      <th scope="col">Zeitraum von</th>
-      <th scope="col">Zeitraum bis</th>
-      <th scope="col">Ort</th>
+      <th class="text-center" scope="col">Zeitraum von</th>
+      <th class="text-center" scope="col">Zeitraum bis</th>
+      <th class="text-center" scope="col">Ort</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>     <?php echo $projektDaten[0]["zeitraum_von"] ?></td>
-      <td>     <?php echo $projektDaten[0]["zeitraum_bis"] ?></td>
-      <td>     <?php echo $projektDaten[0]["ort"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["zeitraum_von"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["zeitraum_bis"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["ort"] ?></td>
     </tr>
   </tbody>
 </table>
+
+<table class="table table-borderless table-active table-vcenter">
+ 
+  <thead>
+    <tr>
+      <th class="text-center" scope="col">Teilnehmerbegrenzung</th>
+      <th class="text-center" scope="col">Jahrgangsstufenbeschränkung</th>
+      <th class="text-center" scope="col">Kosten</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["teilnehmerbegrenzung"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["jahrgangsstufen_beschraenkung"] ?></td>
+      <td style="width:33%" class="text-center">     <?php echo $projektDaten[0]["kosten"] ?></td>
+    </tr>
+  </tbody>
+</table> 
+
 </div>
 <!--<div class="d-flex align-items-start bg-light mb-3" style="height: 25px;">
-  <div class="col"><td>     <?php echo $projektDaten[0]["zeitraum_von"] ?></td></div>
-  <div class="col"><td>     <?php echo $projektDaten[0]["zeitraum_bis"] ?></td></div>
+  <div class="col"><td>     <?php echo $projektDaten[0]["teilnehmerbegrenzung"] ?></td></div>
+  <div class="col"><td>     <?php echo $projektDaten[0]["jahrgangsstufen_beschraenkung"] ?></td></div>
+  <div class="col"><td>     <?php echo $projektDaten[0]["kosten"] ?></td></div>
 </div>-->
 
 
