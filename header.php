bf256633760c297492b6d365df633bb6d3f5923e
Hamza Tamim <hamza.tamim@asg-passau.de>
2022-01-21 09:17:56 +0100

    logout if statement hinzugefügt

--- header.php
+++ header.php
@@ -6,30 +6,35 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 ?>
 
 <div style="float: right; background-color:#fb4400; height: 170% ; width:4%" data-scroll>
-        <input formmethod="post" type="image" id="logout" alt="logout" src="uploads\Test\Logout Logo v2.png" style="width: 100%;"> 
+    <input formmethod="post" type="image" id="logout" alt="logout" src="uploads\Test\Logout Logo v2.png" style="width: 100%;"> 
+</div>
+
+<nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light" style="height: 10ch;">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwzB-PnY6KPKMhQxP9mBPsWxX29ESb72pGgQ&usqp=CAU" class="rounded float-right mg-fluid" style="width: 5%;">
+    <div class="container-fluid">
+        <ul class="navbar-nav mr-auto" style="font-size: 2.5ch; padding">
+            <li class="nav-item active">
+                <a class="nav-item nav-link" href="index.php">Übersicht</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-item nav-link" href="create.php">Erstellen</a>
+            </li>
+            <?php // if($_SESSION['rolle'] == "admin"){ ?>
+            <li class="nav-item active">
+                <a class="nav-item nav-link" href="admin.php">Verwaltung</a>
+            </li>
+            <?php /*}
+                 else{
+                header("Location:login.php");
+                    exit(); 
+                } */
+            ?>
+        </ul>
     </div>
+</nav>
+<?php 
+if(isset($_POST["logout"])){     
+    
+}
+?>
 
-    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light" style="height: 10ch;">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwzB-PnY6KPKMhQxP9mBPsWxX29ESb72pGgQ&usqp=CAU" class="rounded float-right mg-fluid" style="width: 5%;">
-        <div class="container-fluid">
-            <ul class="navbar-nav mr-auto" style="font-size: 2.5ch; padding">
-                <li class="nav-item active">
-                    <a class="nav-item nav-link" href="index.php">Übersicht</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-item nav-link" href="create.php">Erstellen</a>
-                </li>
-                <?php // if($_SESSION['rolle'] == "admin"){ ?>
-                <li class="nav-item active">
-                    <a class="nav-item nav-link" href="admin.php">Verwaltung</a>
-                </li>
-                <?php /*}
-                     else{
-                        header("Location:login.php");
-                        exit(); 
-                    } */
-                ?>
-            </ul>
-        </div>
-
-    </nav>
