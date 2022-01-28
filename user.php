6d8c8f80d33311c35e0cfb378e2a1ee75e319d75
Hamza Tamim <hamza.tamim@asg-passau.de>
2022-01-28 09:31:01 +0100

    .

--- user.php
+++ user.php
@@ -27,27 +27,39 @@
             <script src="bootstrap5-dropdown-ml-hack.js"></script>
         
     </head>
-
+    <div>
 <?php 
 if(isset($_POST["ID"])){ 
     $ergebnis = $db->zeigeEinSchüler($_POST["ID"]);
     $benutzer = $ergebnis[0];
-    echo $benutzer['name'];
-    echo "<form = 'post' action = 'user.php'>".
-            "<input name = 'name' type = 'text' placeholder = ".$benutzer["name"]." >".
-            "<input name = 'name' type = 'text' placeholder = ".$benutzer["klasse"]." >".
-            "<input name = 'name' type = 'text' placeholder = ".$benutzer["rolle"]." >".
-            "</form>";
-    echo "<br><br>
-        <table class='table border shadow p-3' style='margin: auto;'><tr><th>Benutzer ID</th><th>Name</th><th>Klasse</th><th>Rolle</th><th></th></tr>";
-    echo "<tr><td>".$benutzer["benutzer_id"].
-        "</td><td>".$benutzer["name"].
-        "</td><td>".$benutzer["klasse"].
-        "</td><td>".$benutzer["rolle"]."</td><td>";
-    echo "</table></form>";
+    echo "<br><br><br>
+        <form = 'post' action = 'user.php' style='width: 50%; margin: auto'>
+            <table class='table border shadow p-3' style='margin: auto;'>
+                <tr><th>Benutzer ID</th><th>".$benutzer['benutzer_id']."</th><th>".$benutzer['benutzer_id']."</th>
+                <tr><th>Name</th><td>".$benutzer['name']."</td><td><input name = 'name' type = 'text' placeholder = ".$benutzer["name"]."></td>
+                <tr><th>Klasse</th><td>".$benutzer['klasse']."</td><td><input name = 'klasse' type = 'text' placeholder = ".$benutzer["klasse"]."></td>
+                <tr><th>Rolle</th><td>".$benutzer['rolle']."</td><td><input name = 'rolle' type = 'text' placeholder = ".$benutzer["rolle"]."></td>
+            </table>
+            <br>
+            <input type='hidden' name = 'ID' value=".$benutzer["benutzer_id"]."></input>
+            <input formmethod='post' type='submit' class='btn btn-light' name='bestätigen' value='bestätigen'></input>
+        </form>";
+}
+if (isset($_POST['bestätigen'])){
+    $ergebnis = $db->zeigeEinSchüler($_POST["ID"]);
+    $benutzer = $ergebnis[0];
+    if (strlen($_POST['name'])){ $name = $_POST['name'];}
+    else { $name = $benutzer['name'];}
+    
+    if (strlen($_POST['klasse'])){ $klasse = $_POST['klasse']; }
+    else { $klasse = $benutzer['klasse']; }
+    
+    if (strlen($_POST['rolle'])){ $rolle = $_POST['rolle']; }
+    else { $rolle = $benutzer['rolle']; }
+
+    echo $_POST["ID"], $name, $klasse, $rolle;
+    $db->updateBenutzer($_POST["ID"], $name, $klasse, $rolle);
 }
 ?>
-    <body>
-        
-    </body>
+    </div>
 </html>
